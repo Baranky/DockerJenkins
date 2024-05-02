15daa0da111d0eed7ae88b4003bf2f025a62cce1
Baran Kaya <99399601+Baranky@users.noreply.github.com>
2024-05-02 14:21:34 +0300

    f

--- src/main/java/com/example/jenkins/controler/CustomerController.java
+++ src/main/java/com/example/jenkins/controler/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
     public class CustomerController {
         @GetMapping()
         public String home() {
-            return "Hello, Worgrgjkad";
+            return "BARAN BABA 44 31 52";
 
         }
     }
